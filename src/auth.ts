0e05b4fa1c862056488ee572b0b1f20b7d2c5bef
fdelu <fdeluca@fi.uba.ar>
2024-04-28 20:56:56 -0300

    Check token in local storage for auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,26 +2,32 @@ import { AuthProvider } from "react-admin";
 import axios from "axios";
 import { USERS_API_URL } from "./util";
 
+const axiosHeaders = axios.defaults.headers.common;
+
 const authProvider: AuthProvider = {
   login: async ({ username, password }) => {
     const response = await axios.post(`${USERS_API_URL}/login`, {
       email: username,
       password,
     });
-    axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+    axiosHeaders.Authorization = `Bearer ${response.data.token}`;
     localStorage.setItem("userId", response.data.id);
+    localStorage.setItem("token", response.data.token);
   },
   logout: async () => {
     localStorage.removeItem("userId");
-    if (axios.defaults.headers.common.Authorization) {
+    localStorage.removeItem("token");
+    if (axiosHeaders.Authorization) {
       await axios.post(`${USERS_API_URL}/logout`);
-      delete axios.defaults.headers.common.Authorization;
+      delete axiosHeaders.Authorization;
     }
   },
   checkAuth: async () => {
-    return axios.defaults.headers.common.Authorization
-      ? Promise.resolve()
-      : Promise.reject();
+    if (!axiosHeaders.Authorization) {
+      const token = localStorage.getItem("token");
+      if (token) axiosHeaders.Authorization = `Bearer ${token}`;
+    }
+    return axiosHeaders.Authorization ? Promise.resolve() : Promise.reject();
   },
   checkError: async (error: unknown) => {
     if (
